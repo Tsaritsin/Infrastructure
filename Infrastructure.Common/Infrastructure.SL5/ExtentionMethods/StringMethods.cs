867069ced546430d73e486a5d9070ab99ac9ecc1
Tsaritsin <tsaritsin.joy@gmail.com>
2017-09-24 20:34:09 +0300

    Added project for SL5 to Infrastructure.Common;

---
+++ Infrastructure.Common/Infrastructure.SL5/ExtentionMethods/StringMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Harmony.Infrastructure.Common.ExtentionMethods
+{
+	/// <summary>
+	/// Contains extention method for string's value
+	/// </summary>
+	public static class StringMethods
+	{
+		/// <summary>
+		/// Implement functional of method <see cref="String.IsNullOrEmpty"/>.
+		/// </summary>
+		/// <param name="value">The string to test.</param>
+		/// <returns></returns>
+		public static bool IsNullOrEmpty(this string value)
+		{
+			var result = String.IsNullOrEmpty(value);
+			return result;
+		}
+
+		/// <summary>
+		/// Throw exception is value equals null or Empty.
+		/// </summary>
+		/// <param name="value">The string to test.</param>
+		/// <param name="exception">The exception to throw.</param>
+		public static string ThrowIfIsNullOrEmpty(this string value, Exception exception)
+		{
+			if (String.IsNullOrEmpty(value))
+				throw exception;
+			return value;
+		}
+
+		/// <summary>
+		/// Throw exception (InvalidOperationException) is value equals null or Empty.
+		/// </summary>
+		/// <param name="value">The string to test.</param>
+		/// <param name="errorMessage">The message for exception to throw.</param>
+		public static string ThrowIfIsNullOrEmpty(this string value, string errorMessage)
+		{
+			if (String.IsNullOrEmpty(value))
+				throw new InvalidOperationException(errorMessage);
+			return value;
+		}
+
+		/// <summary>
+		/// Throw exception (ArgumentException) if argument is equals null or Empty.
+		/// </summary>
+		/// <param name="value">The argument to test.</param>
+		/// <param name="argumentName">Name of argument to test.</param>
+		public static string ThrowIfArgumentIsNullOrEmpty(this string value, string argumentName)
+		{
+			if (String.IsNullOrEmpty(value))
+				throw new ArgumentNullException($"Argument [{argumentName}] is empty");
+			return value;
+		}
+	}
+}
