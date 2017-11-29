f3e9f1b8f227ae700ae62fc856759e0ec5e43d3f
Tsaritsin <tsaritsin.joy@gmail.com>
2017-11-29 21:34:54 +0200

    Added MVVM.NET40

---
+++ Infrastructure.MVVM/Infrastructure.NET40/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Harmony.Infrastructure.MVVM
+{
+	public class DelegateCommand : ICommand
+	{
+		#region Fields
+
+		
+
+		#endregion
+
+		#region Implementation ICommand
+
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Execute(object parameter)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
