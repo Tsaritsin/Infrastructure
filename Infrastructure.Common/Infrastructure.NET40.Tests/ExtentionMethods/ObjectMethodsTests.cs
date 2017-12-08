ba58603ad1cee677d74ec1918c3e979d3f54ea55
Tsaritsin <tsaritsin.joy@gmail.com>
2017-12-08 17:59:50 +0200

    Added ObjectMethods;

---
+++ Infrastructure.Common/Infrastructure.NET40.Tests/ExtentionMethods/ObjectMethodsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Harmony.Infrastructure.Common.ExtentionMethods;
+using NUnit.Framework;
+
+namespace Infrastructure.Common.NET40.Tests.ExtentionMethods
+{
+	[TestFixture]
+	public class ObjectMethodsTests
+	{
+		[TestCase(null, "TestErrorMessage1", typeof(Exception), TestName = "ValueIsEmptyStringWaitException")]
+		[TestCase("TestValue", null, null, TestName = "WithoutException")]
+		[TestCase(null, "TestErrorMessage2", typeof(NullReferenceException), TestName = "WaitNullReferenceException")]
+		public void SomeValue_CallThrowIfIsNull_GetException(
+			object value, string errorMessage, Type expectedExceptionType)
+		{
+			#region Arrange
+
+			Exception expectedException = null;
+			if (expectedExceptionType != null)
+				expectedException = (Exception)Activator.CreateInstance(expectedExceptionType, errorMessage);
+
+			TestDelegate actionForTest = delegate
+			{
+				value.ThrowIfIsNull(expectedException);
+			};
+
+			#endregion
+
+			#region Assert
+
+			if (expectedExceptionType == null)
+				Assert.DoesNotThrow(actionForTest);
+			else
+			{
+				var actualExeption = Assert.Throws(Is.TypeOf(expectedExceptionType), actionForTest);
+				Assert.That(actualExeption.Message, Is.EqualTo(errorMessage));
+			}
+
+			#endregion
+		}
+
+		[TestCase("TestValue", false, TestName = "ValueIsSampleString")]
+		[TestCase(null, true, TestName = "ValueIsNull")]
+		public void SomeValue_CallThrowIfArgumentIsNull_GetArgumentNullException(object value, bool exceptionIsExpected)
+		{
+			#region Arrange
+
+			const string argumentName = "Argument1";
+
+			TestDelegate actionForTest = delegate
+			{
+				value.ThrowIfArgumentIsNull(argumentName);
+			};
+
+			#endregion
+
+			#region Assert
+
+			if (exceptionIsExpected)
+			{
+				var actualExeption = Assert.Throws<ArgumentNullException>(actionForTest);
+
+				StringAssert.Contains(argumentName, actualExeption.Message);
+			}
+			else
+				Assert.DoesNotThrow(actionForTest);
+
+			#endregion
+		}
+
+	}
+}
---
+++ Infrastructure.Common/Infrastructure.NET40/ExtentionMethods/ObjectMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Harmony.Infrastructure.Common.ExtentionMethods
+{
+	/// <summary>
+	/// Contains extention method for object's value
+	/// </summary>
+	public static class ObjectMethods
+	{
+		/// <summary>
+		/// Throw exception is value equals null.
+		/// </summary>
+		/// <param name="value">The object to test.</param>
+		/// <param name="exception">The exception to throw.</param>
+		public static T ThrowIfIsNull<T>(this T value, Exception exception)
+		{
+			if (value == null)
+				throw exception;
+			return value;
+		}
+
+		/// <summary>
+		/// Throw exception (ArgumentException) if argument is equals null
+		/// </summary>
+		/// <param name="value">The argument to test.</param>
+		/// <param name="argumentName">Name of argument to test.</param>
+		public static T ThrowIfArgumentIsNull<T>(this T value, string argumentName)
+		{
+			if (value == null)
+				throw new ArgumentNullException($"Argument [{argumentName}] is null");
+			return value;
+		}
+	}
+}
